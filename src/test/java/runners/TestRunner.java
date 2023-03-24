31ec6a1f943a93a86c21fbe9017fe16598a3fc96
vinay <vinayagamaruthiswamy@gmail.com>
2023-03-24 11:53:53 +0530

    testrunner modified

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -10,8 +10,9 @@ import io.cucumber.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features ="src/test/resources/functionalTests",
-		glue= {"stepDefinitons"},
-			plugin = {"json:target/cucumber.json"}
+		glue= {"stepDefinitons"}
+//			plugin = {"json:target/cucumber.json"},
+//			tags="@api"
 //		"io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm"
 		)
 public class TestRunner {
